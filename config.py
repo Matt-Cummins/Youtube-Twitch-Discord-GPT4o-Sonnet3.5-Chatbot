4adc5e52a40158c458178f6952467f240c00826e
Matthew Cummins <79932599+Matt-Cummins@users.noreply.github.com>
2024-06-23 19:36:13 +0100

    Update config.py
    Signed-off-by: Matthew Cummins <79932599+Matt-Cummins@users.noreply.github.com>

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-# config.py
 import os
 
 class Config:
@@ -35,14 +34,12 @@ class Config:
         cls.DISCORD_BOT_TOKEN = cls.get_env_variable('DISCORD_BOT_TOKEN', required=True)
         cls.TWITCH_CLIENT_ID = cls.get_env_variable('TWITCH_CLIENT_ID', required=True)
         cls.TWITCH_CLIENT_SECRET = cls.get_env_variable('TWITCH_CLIENT_SECRET', required=True)
-        cls.OPENAI_API_KEY = cls.get_env_variable('OPENAI_KEY', required=True)
+        cls.OPENAI_API_KEY = cls.get_env_variable('OPENAI_API_KEY', required=True)
         cls.GOOGLE_PSE_ID = cls.get_env_variable('GOOGLE_PSE_ID', required=True)
         cls.GOOGLE_PSE_API_KEY = cls.get_env_variable('GOOGLE_PSE_API_KEY', required=True)
         cls.TWITCH_BOT_NAME = cls.get_env_variable('TWITCH_BOT_NAME', 'defaultBotName')
         cls.TWITCH_BOT_TOKEN = cls.get_env_variable('TWITCH_BOT_TOKEN', required=True)
         cls.TWITCH_CHANNEL_NAME = cls.get_env_variable('TWITCH_CHANNEL_NAME', 'defaultChannelName')
-        cls.ACCESS_TOKEN = None
-        cls.TOKEN_EXPIRY = 0
         cls.YOUTUBE_API_KEY = cls.get_env_variable('YOUTUBE_API_KEY', required=True)
         cls.YOUTUBE_ACCESS_TOKEN = cls.get_env_variable('YOUTUBE_ACCESS_TOKEN', required=True)
         cls.YOUTUBE_LIVE_CHAT_ID = cls.get_env_variable('YOUTUBE_LIVE_CHAT_ID', required=True)
@@ -52,3 +49,10 @@ class Config:
         cls.OPENWEATHERMAP_API_KEY = cls.get_env_variable('OPENWEATHERMAP_API_KEY', required=True)
         cls.AVIATION_EDGE_API_KEY = cls.get_env_variable('AVIATION_EDGE_API_KEY', required=True)
         cls.NAVIGRAPH_API_KEY = cls.get_env_variable('NAVIGRAPH_API_KEY', required=True)
+        cls.ACCESS_TOKEN = None
+        cls.TOKEN_EXPIRY = 0
+
+# Example usage
+if __name__ == "__main__":
+    Config.load_configuration()
+    print(f"Discord Bot Token: {Config.DISCORD_BOT_TOKEN}")
